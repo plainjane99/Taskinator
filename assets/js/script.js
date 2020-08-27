8cdf76dcdde49fa3c0c7fa2319a7d7db760eb790
Plain Jane <jane99hsu@gmail.com>
2020-08-26 21:56:19 -0700

    improved user interface with dragleave

--- assets/js/script.js
+++ assets/js/script.js
@@ -202,9 +202,14 @@ var dragTaskHandler = function(event) {
 
 var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
+
     if (taskListEl) {
         event.preventDefault();
+
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
+
     }
+
 };
 
 var dropTaskHandler = function(event) {
@@ -228,7 +233,20 @@ var dropTaskHandler = function(event) {
         statusSelectEl.selectedIndex = 2;
     }
 
+    dropZoneEl.removeAttribute("style");
+
     dropZoneEl.appendChild(draggableElement);
+};
+
+var dragLeaveHandler = function(event) {
+
+    var taskListEl = event.target.closest(".task-list");
+
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+
+    }
+
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -240,3 +258,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
